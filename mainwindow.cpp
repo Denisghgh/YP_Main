1685ff66578ce5f07391f617d6840b4db90901a9
DenisMarch <denismarhenko123@gmail.com>
2019-11-09 09:36:43 +0300

    2 games
    Snake + Doodle Jump + My YP 5 Labe (circles)
    
    3 buttons

---
+++ mainwindow.cpp
@@ -0,0 +1,60 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <iostream>
+#include <windows.h>
+#include <QDir>
+#include "ShellAPI.h"
+#include <QProcess>
+#include <windows.h>
+#include <objbase.h>
+
+void Circles()
+{
+QProcess Circle;
+Circle.start("labexample\\Debug\\labexample.exe");
+Circle.waitForFinished(-1);
+}
+
+void Snake_Game()
+{
+//system("start C:\\Trash\\Course_YP\\Games\\Snake\\Snake1\\x64\\Debug\\Snake1.exe");
+QProcess Snake_Game;
+Snake_Game.start("Games\\Snake\\Snake1\\x64\\Debug\\Snake1.exe");
+Snake_Game.waitForFinished(-1);
+}
+
+void DoodleJ()
+{
+QProcess DoodleJ;
+DoodleJ.start("Games\\DoodleJ\\x64\\Debug\\DoodleJ.exe");
+DoodleJ.waitForFinished(-1);
+}
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_b1_clicked()
+{
+    Circles();
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    Snake_Game();
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    DoodleJ();
+}
